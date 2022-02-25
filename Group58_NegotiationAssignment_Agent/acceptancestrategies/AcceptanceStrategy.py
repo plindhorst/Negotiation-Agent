609f3291dbfdb5c2b8b28dd30e3e5539edb5b264
balys_morkunas <balys.morkunas@gmail.com>
2022-02-25 13:21:29 +0100

    fix accept strat to check for opponent bid

--- Group58_NegotiationAssignment_Agent/acceptancestrategies/AcceptanceStrategy.py
+++ Group58_NegotiationAssignment_Agent/acceptancestrategies/AcceptanceStrategy.py
@@ -10,11 +10,12 @@ class AcceptanceStrategy:
         # 80% of the rounds towards the deadline have passed
 
         return (
-                self._accept_next(opponent_bid, my_bid)
-                or self._profile.getUtility(my_bid) > self._alpha
-                and progress > 0.8
+            self._accept_next(opponent_bid, my_bid)
+            or self._profile.getUtility(opponent_bid) > self._alpha
+            and progress > 0.8
         )
 
     def _accept_next(self, opponent_bid, my_bid):
-        return opponent_bid is not None and self._profile.getUtility(opponent_bid) > self._profile.getUtility(my_bid)
-
+        return opponent_bid is not None and self._profile.getUtility(
+            opponent_bid
+        ) > self._profile.getUtility(my_bid)
